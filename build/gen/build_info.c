4934fc341a756f6958479df9752a929e37cc716a
U-admin-PC\admin <ngoc.lo.dang@gmail.com>
2018-08-08 00:45:15 +0700

    build

---
+++ build/gen/build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *build_id = "20180807-155448/???";
+const char *build_timestamp = "2018-08-07T15:54:48Z";
+const char *build_version = "1.0";
---
+++ build/gen/mg_build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *mg_build_id = "20180807-155448/???";
+const char *mg_build_timestamp = "2018-08-07T15:54:48Z";
+const char *mg_build_version = "2018080715";
